5f47a9c3537eccd712752a2fc8fec12a9c2f48ff
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2024-03-18 20:41:55 +0000

    script: update script

--- script.js
+++ script.js
@@ -1,10 +1,89 @@
-import { readFile } from 'node:fs/promises';
+import * as fs from "node:fs/promises";
 
-const filePath = './indic-dicts-index/dictionaryIndices.md';
+const filePath = "./indic-dicts-index/dictionaryIndices.md";
+
+const printProgress = (progress) => {
+  process.stdout.clearLine(0);
+  process.stdout.cursorTo(0);
+  process.stdout.write(progress);
+};
+
+const getDictUrls = async (dictionaryReposURLs) => {
+  const dictUrls = [];
+
+  for (const [index, repoURL] of dictionaryReposURLs.entries()) {
+    printProgress(`Fetching ${index + 1} of ${dictionaryReposURLs.length}`);
+
+    const response = await fetch(repoURL);
+
+    if (response.ok) {
+      const data = await response.text();
+
+      const dictUrlsInRepo = data.split("\n").filter((line) => line !== "");
+
+      dictUrls.push(...dictUrlsInRepo);
+    }
+  }
+
+  return dictUrls;
+};
+
+const parseDictReposIndex = (dictReposIndex) => {
+  return dictReposIndex
+    .split("\n")
+    .filter((line) => line.startsWith("<"))
+    .map((line) => line.replace(/[<>]/g, "").trim())
+    .filter((line) => line !== "");
+};
+
+const getDictDetailsFromURL = (dictURL, dictURLRegex) => {
+  const match = dictURL.match(dictURLRegex);
+
+  if (match == null) return null;
+
+  const name = match.at(3) ?? "";
+  const category = match.at(1) ?? "";
+  const subcategory = match.at(2)?.replace(/\/$/, "") ?? "";
+  const timestamp = match.at(4) ?? "";
+  const size = match.at(5) ?? "";
+  const url = encodeURI(match.at(0) ?? "");
+  const fileName = `${name}__${timestamp}`;
+
+  return {
+    name,
+    category,
+    subcategory,
+    timestamp,
+    size,
+    url,
+    fileName,
+  };
+};
+
+const parseDictUrls = (dictURLs) => {
+  const dictURLRegex =
+    /https:\/\/github.com\/indic-dict\/stardict-(.*?)\/raw\/gh-pages\/(.*?)tars\/(.*?)__(.*?)__(.*?).tar.gz/;
+
+  // TODO: handle dict urls that do not conform to `dictionaryURLRegex`
+  const dictDetailsList = dictURLs
+    .map((dictURL) => getDictDetailsFromURL(dictURL, dictURLRegex))
+    .filter((dictDetails) => dictDetails !== null);
+
+  return dictDetailsList;
+};
 
 try {
-  const content = await readFile(filePath, 'utf-8');
-  console.log(content);
+  const dictReposIndex = await fs.readFile(filePath, "utf-8");
+
+  const dictReposUrls = parseDictReposIndex(dictReposIndex);
+
+  const dictUrls = await getDictUrls(dictReposUrls);
+
+  const dictsList = parseDictUrls(dictUrls);
+
+  const dictsListJson = JSON.stringify(dictsList, null, 4);
+
+  await fs.writeFile("dicts_list.json", dictsListJson, "utf-8");
 } catch (err) {
   console.error(err);
 }
